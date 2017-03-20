4a7e0acdd9ccfe5ac0b40cc0cc91cb38fec2b5c9
ripper121 <sts@zwave.eu>
2017-03-20 15:39:05 +0100

    Added Flash Light Alarm

--- FishAlarm/app/src/main/java/uk23/ripper121/com/fishalarm/MainActivity.java
+++ FishAlarm/app/src/main/java/uk23/ripper121/com/fishalarm/MainActivity.java
@@ -1,15 +1,15 @@
 package uk23.ripper121.com.fishalarm;
 
-import android.app.Service;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Camera;
+import android.media.RingtoneManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.app.Activity;
-import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Gravity;
@@ -18,26 +18,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.apache.http.conn.util.InetAddressUtils;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
-import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Enumeration;
-import java.util.Formatter;
-import java.util.List;
 
 
 public class MainActivity extends Activity {
+    private final static String TAG = MainActivity.class.getSimpleName();
     public TextView text1;
     public TextView text2;
     public ScrollView mScrollView;
-    public static Handler mUiHandler = null;
+    public TextView textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +44,9 @@ public class MainActivity extends Activity {
         text1.setMovementMethod(new ScrollingMovementMethod());
         text2 = (TextView) findViewById(R.id.textView2);
         text2.setGravity(Gravity.CENTER_HORIZONTAL);
-        text2.setText("IP: " + getIpAddress() + " Port:8080");
 
+        textView = (TextView) findViewById(R.id.textView1);
+        mScrollView = (ScrollView) findViewById(R.id.SCROLLER_ID);
 
         final Intent service = new Intent(this, MyService.class);
 
@@ -66,24 +64,79 @@ public class MainActivity extends Activity {
             }
         });
 
-        mScrollView = (ScrollView) findViewById(R.id.SCROLLER_ID);
-        mUiHandler = new Handler() // Receive messages from service class
-        {
-            public void handleMessage(Message msg)
-            {
-                switch(msg.what)
-                {
-                    case 0:
-                        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
-                        text1.append(currentDateTimeString + " // " + msg.obj.toString() + "\n");
-                        scrollToBottom();
-                        break;
+        final Button buttonAlarm = (Button) findViewById(R.id.buttonAlarm);
+        buttonAlarm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                alarmOff();
+            }
+        });
 
-                    default:
-                        break;
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        String text = intent.getStringExtra(MyService.EXTRA_TEXT);
+                        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                        textView.append(currentDateTimeString + " // " + text + "\n");
+                        scrollToBottom();
+                    }
+                }, new IntentFilter(MyService.ACTION_LOCATION_BROADCAST)
+        );
+
+        WifiManager wifi = (WifiManager) MainActivity.this.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        Method[] wmMethods = wifi.getClass().getDeclaredMethods();
+        for (Method method : wmMethods) {
+            if (method.getName().equals("isWifiApEnabled")) {
+
+
+                try {
+                    boolean isWifiAPenabled = (Boolean) method.invoke(wifi);
+                    if (isWifiAPenabled)
+                        text2.setText("IP: " + getWifiApIpAddress() + " Port:8080");
+                    else
+                        text2.setText("IP: " + getIpAddress() + " Port:8080");
+                } catch (Exception e) {
+                    String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                    textView.append(currentDateTimeString + " // " + e.getMessage() + "\n");
+                    scrollToBottom();
                 }
             }
-        };
+        }
+    }
+
+    void alarmOff() {
+        try {
+            RingtoneManager.getRingtone(MainActivity.this, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)).stop();
+
+            Camera camera = null;
+            Camera.Parameters params = null;
+            if (camera == null) {
+                try {
+                    camera = Camera.open();
+                    params = camera.getParameters();
+                } catch (RuntimeException e) {
+                    String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                    textView.append(currentDateTimeString + " // " + e.getMessage() + "\n");
+                    scrollToBottom();
+                }
+            }
+            if (camera == null || params == null) {
+                return;
+            }
+            params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(params);
+            camera.stopPreview();
+
+            String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+            textView.append(currentDateTimeString + " // Alarm turned Off!\n");
+            scrollToBottom();
+
+        } catch (Exception e) {
+            String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+            textView.append(currentDateTimeString + " // " + e.getMessage() + "\n");
+            scrollToBottom();
+        }
     }
 
 
@@ -117,13 +170,33 @@ public class MainActivity extends Activity {
         return ip;
     }
 
+    public String getWifiApIpAddress() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en
+                    .hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                if (intf.getName().contains("wlan")) {
+                    for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr
+                            .hasMoreElements(); ) {
+                        InetAddress inetAddress = enumIpAddr.nextElement();
+                        if (!inetAddress.isLoopbackAddress()
+                                && (inetAddress.getAddress().length == 4)) {
+                            Log.d(TAG, inetAddress.getHostAddress());
+                            return inetAddress.getHostAddress();
+                        }
+                    }
+                }
+            }
+        } catch (SocketException ex) {
+            Log.e(TAG, ex.toString());
+        }
+        return null;
+    }
 
-    private void scrollToBottom()
-    {
-        mScrollView.post(new Runnable()
-        {
-            public void run()
-            {
+
+    private void scrollToBottom() {
+        mScrollView.post(new Runnable() {
+            public void run() {
                 mScrollView.smoothScrollTo(0, text1.getBottom());
             }
         });
--- FishAlarm/app/src/main/java/uk23/ripper121/com/fishalarm/MyService.java
+++ FishAlarm/app/src/main/java/uk23/ripper121/com/fishalarm/MyService.java
@@ -1,39 +1,48 @@
 package uk23.ripper121.com.fishalarm;
-import android.app.IntentService;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Camera;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Process;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
-import java.util.Date;
 
-public class MyService extends Service{
-
+public class MyService extends Service {
 
 
     private final static String TAG = MainActivity.class.getSimpleName();
     static final int UdpServerPORT = 8080;
     private boolean serverRunning = true;
-    DatagramSocket socket;
+    private DatagramSocket socket;
     public static NotificationManager notificationManager = null;
-
     private Looper mServiceLooper;
     private ServiceHandler mServiceHandler;
+
+    public static final String ACTION_LOCATION_BROADCAST = MainActivity.class.getName() + "Broadcast", EXTRA_TEXT = "extra_text";
+
+    private void sendBroadcastMessage(String text) {
+        Intent intent = new Intent(ACTION_LOCATION_BROADCAST);
+        intent.putExtra(EXTRA_TEXT, text);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
 
     @Override
     public void onCreate() {
@@ -46,39 +55,32 @@ public class MyService extends Service{
         // start the service using the background handler
         mServiceHandler = new ServiceHandler(mServiceLooper);
         serverRunning = true;
-        generateNotification(MyService.this, "Fish Alarm Running",0);
+        sendBroadcastMessage("Service created");
     }
 
     @Override
     public void onDestroy() {
         notificationManager.cancel(0);
         serverRunning = false;
+        generateNotification(MyService.this, "Fish Alarm Running", 0, false);
+        notificationManager.cancelAll();
+        sendBroadcastMessage("Service stopped");
         super.onDestroy();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "onStartCommand", Toast.LENGTH_SHORT).show();
 
         // call a new service handler. The service ID can be used to identify the service
         Message message = mServiceHandler.obtainMessage();
         message.arg1 = startId;
         mServiceHandler.sendMessage(message);
-        generateNotification(MyService.this, "New Fish!",0);
+
+        generateNotification(MyService.this, "Fish Alarm Running", 0, true);
+        sendBroadcastMessage("Service is running");
         return START_STICKY;
     }
 
-    protected void showToast(final String msg){
-        //gets the main thread
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                // run this code in the main thread
-                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -88,98 +90,141 @@ public class MyService extends Service{
     // Object responsible for
     private final class ServiceHandler extends Handler {
 
+
         public ServiceHandler(Looper looper) {
             super(looper);
         }
 
         @Override
         public void handleMessage(Message msg) {
+
             // Well calling mServiceHandler.sendMessage(message); from onStartCommand,
             // this method will be called.
 
             // Add your cpu-blocking activity here
-
-            showToast("Starting IntentService");
+            sendBroadcastMessage("Server starting...");
 
             try {
                 //updateState("Starting UDP Server");
                 socket = new DatagramSocket(UdpServerPORT);
-
+                sendBroadcastMessage("Server socket created");
                 //updateState("UDP Server is running");
                 Log.e(TAG, "UDP Server is running");
 
-                while(serverRunning){
-                    byte[] buf = new byte[256];
-
+                sendBroadcastMessage("Server listening");
+                while (serverRunning) {
+                    byte[] buf = new byte[32];
                     // receive request
                     DatagramPacket packet = new DatagramPacket(buf, buf.length);
-
                     socket.receive(packet);     //this code block the program flow
 
-                    // send the response to the client at "address" and "port"
-                    InetAddress address = packet.getAddress();
-                    int port = packet.getPort();
-
-                    //updatePrompt("Request from: " + address + ":" + port + "\n");
-
-                    String dString = new Date().toString() + "\n"
-                            + "Your address " + address.toString() + ":" + String.valueOf(port);
-                    buf = dString.getBytes();
-                    packet = new DatagramPacket(buf, buf.length, address, port);
-                    socket.send(packet);
-                    generateNotification(MyService.this, "New Fish!",1);
-
+                    String msgPacket = new String(packet.getData(), packet.getOffset(), packet.getLength());
+                    sendBroadcastMessage(msgPacket);
+                    if (msgPacket.contains("Alarm"))
+                        generateNotification(MyService.this, "New Fish!", 1, false);
                 }
-
+                sendBroadcastMessage("Server stopped");
                 Log.e(TAG, "UDP Server ended");
-
             } catch (SocketException e) {
                 e.printStackTrace();
+                sendBroadcastMessage(e.getMessage());
             } catch (IOException e) {
                 e.printStackTrace();
+                sendBroadcastMessage(e.getMessage());
             } finally {
-                if(socket != null){
+                if (socket != null) {
                     socket.close();
                     Log.e(TAG, "socket.close()");
+                    sendBroadcastMessage("Server socket closed");
                 }
             }
-            showToast("Finishing Service, id: " + msg.arg1);
             // the msg.arg1 is the startId used in the onStartCommand, so we can track the running sevice here.
             stopSelf(msg.arg1);
         }
     }
 
-    private static void generateNotification(Context context, String message, Integer id) {
+    private void generateNotification(Context context, String message, Integer id, boolean onGoing) {
         int icon = R.drawable.notification_template_icon_bg;
         long when = System.currentTimeMillis();
-        notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         Notification notification = new Notification(icon, message, when);
 
         String title = "Fish Alarm!";
 
         Intent notificationIntent = new Intent(context, MainActivity.class);
-        // set intent so it does not start a new activity
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent intent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
-        notification.setLatestEventInfo(context, title, message, intent);
-        if(id==0) {
-            notification.flags |= Notification.FLAG_ONGOING_EVENT;
-        }else {
-            notification.ledARGB = 0xFF0000FF;
-            notification.ledOnMS = 1000;
-            notification.ledOffMS = 300;
-            notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+        try {
+            if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB) {
+                // set intent so it does not start a new activity
+                notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                PendingIntent intent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+                notification.setLatestEventInfo(context, title, message, intent);
+                if (onGoing) {
+                    notification.flags |= Notification.FLAG_ONGOING_EVENT;
+                } else {
+                    notification.ledARGB = 0xFF0000FF;
+                    notification.ledOnMS = 1000;
+                    notification.ledOffMS = 300;
+                    notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+                    notification.defaults |= Notification.DEFAULT_VIBRATE;
+                    notification.defaults |= Notification.PRIORITY_MAX;
+                }
 
-            // Play default notification sound
-            notification.defaults |= Notification.DEFAULT_SOUND;
-            // Vibrate if vibrate is enabled
-            notification.defaults |= Notification.DEFAULT_VIBRATE;
-
-            notification.defaults |= Notification.PRIORITY_MAX;
+            } else {
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+                notification = builder
+                        .setOngoing(onGoing)
+                        .setLights(0xFF0000FF, 1000, 300)
+                        .setDefaults(Notification.DEFAULT_VIBRATE | Notification.PRIORITY_MAX)
+                        .setSmallIcon(icon)
+                        .setTicker(message)
+                        .setWhen(when)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .build();
+            }
+            notificationManager.notify(id, notification);
+        } catch (Exception e) {
+            sendBroadcastMessage(e.getMessage());
         }
-        notificationManager.notify(id, notification);
+
+        if (id > 0) {
+
+            try {
+                RingtoneManager.getRingtone(context, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)).play();
+            } catch (Exception e) {
+                sendBroadcastMessage(e.getMessage());
+            }
+
+            try {
+                turnOnFlash();
+            } catch (Exception e) {
+                sendBroadcastMessage(e.getMessage());
+            }
+        }
     }
 
+    private void turnOnFlash() {
+        Camera camera = null;
+        Camera.Parameters params  = null;
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                params = camera.getParameters();
+            } catch (RuntimeException e) {
+                sendBroadcastMessage(e.getMessage());
+            }
+        }
+
+        if (camera == null || params == null) {
+            return;
+        }
+
+        params = camera.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(params);
+        camera.startPreview();
+
+    }
 
 
 }
